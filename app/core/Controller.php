0808e5084fc467c1328f339ad20529e1d5754987
Neville-Owen <nevilleowen2009@gmail.com>
2026-04-17 11:10:04 +0700

    feat: implement student index php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,18 +1,19 @@
 <?php
 namespace App\core;
-
+ 
 class Controller
 {
     public function view(string $view, array $data = [])
     {
+        extract($data);
         $view = str_replace(
             '.',
             '/',
             $view
         );
-
+ 
         $content = "../app/views/$view.php";
-
+ 
         require_once '../app/views/layouts/app.php';
     }
 }
